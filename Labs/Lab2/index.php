bd1cb6f99a05fb6c847168a9352f4c10b8244c08
Lesley Amezcua <lamezcua@csumb.edu>
2018-02-19 22:32:38 +0000

    Added Lab3

---
+++ Labs/Lab2/index.php
@@ -0,0 +1,36 @@
+<?php
+    include 'inc/functions.php';
+?>    
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+        <style type="text/css">
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        <div id="main">
+            <?php
+                play();
+            ?>
+            
+            <form>
+              <input type="submit" value="Spin!"/>
+            </form>
+        </div>    
+        <footer>
+            <br />
+            <br />
+            <br />
+            <br />
+            Image sources: <br>
+            http://www.modern-canvas-art.com/ekmps/shops/robboweb1/images/slot-machine-symbols-pop-art-canvas-print-4252-p.jpg
+            <br />
+            http://www.freeslotmachines.me.uk/
+            <br />
+            http://img.over-blog-kiwi.com/1/18/84/05/20140812/ob_3ea045_slotmachine-wallpaper-777.jpg
+        </footer>
+    </body>
+</html>
